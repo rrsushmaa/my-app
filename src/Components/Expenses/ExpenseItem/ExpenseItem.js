56e3743cf268aa44e37c3e11cc15c0343f887f36
Sushmaa R R <sushmaa.rr@thoughtworks.com>
2022-03-25 07:18:55 +0530

    Remove state from expense item

--- src/Components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,23 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDateItem from "../ExpenseDateItem/ExpenseDateItem";
 import Card from "../../UI/Cards/Card";
-import {useState} from 'react';
 
 const ExpenseItem = (props) => {
 
-    const [title, updateTitle] = useState(props.title);
-    const clickHandler = () => {
-        updateTitle('Updated');
-        console.log(title);
-    }
     return (
         <Card className="expense-item">
             <ExpenseDateItem date={props.date}/>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">₹{props.price}</div>
             </div>
-            <button onClick={clickHandler}> Change Title</button>
         </Card>);
 }
 
