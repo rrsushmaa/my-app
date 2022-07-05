be8208362a35aac16a6d3ca7f7824cd4c9d7c62a
Sushmaa R R <sushmaa.rr@thoughtworks.com>
2022-07-05 09:35:41 +0530

    [#18][Sushmaa]Show and Hide expense form

--- src/Components/Expenses/NewExpense/ExpenseForm.js
+++ src/Components/Expenses/NewExpense/ExpenseForm.js
@@ -32,34 +32,40 @@ const ExpenseForm = (prop) => {
         setNewAmount('');
         setNewDate('');
         setNewTitle('');
+        prop.onCancelExpenseEditing();
+    }
+
+    const cancelHandler = () => {
+        prop.onCancelExpenseEditing();
     }
 
     return (<div>
-            <form onSubmit={submitHandler} onReset={resetHandler}>
-                <div className="new-expense__controls">
-                    <div className="new-expense__control">
-                        <label>Title</label>
-                        <input type="text" value={newTitle} onChange={titleChangeHandler}/>
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Amount</label>
-                        <input type="number" value={newAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Date</label>
-                        <input type="date" value={newDate} min="1970-01-01" max="2024-12-31"
-                               onChange={dateChangeHandler}/>
-                    </div>
+        <form onSubmit={submitHandler} onReset={resetHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" value={newTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
-                    <div className="new-expense__actions">
-                        <button type="submit">Add Expense</button>
-                        <button type="reset">Reset</button>
-                    </div>
-
+                    <label>Amount</label>
+                    <input type="number" value={newAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
-            </form>
-        </div>);
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" value={newDate} min="1970-01-01" max="2024-12-31"
+                           onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className="new-expense__control">
+                <div className="new-expense__actions">
+                    <button type="submit">Add Expense</button>
+                    <button type="reset">Reset</button>
+                    <button type="button" onClick={cancelHandler}>Cancel</button>
+                </div>
+
+            </div>
+        </form>
+    </div>);
 }
 
 export default ExpenseForm;
--- src/Components/Expenses/NewExpense/NewExpense.js
+++ src/Components/Expenses/NewExpense/NewExpense.js
@@ -1,17 +1,37 @@
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
+import {useState} from "react";
 
 const NewExpense = (prop) => {
+    const [showExpenseForm, setShowExpenseForm] = useState(false);
     const saveExpenseHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         prop.onSubmitNewExpense(expenseData);
+        setShowExpenseForm(false);
+
     };
+    const hideExpenseForm = () => {
+        setShowExpenseForm(false);
+    }
+    let expenseForm = <ExpenseForm onSaveExpenseData={saveExpenseHandler} onCancelExpenseEditing={hideExpenseForm}/>;
+
+    const handleOnExpenseAddition = () => {
+        setShowExpenseForm(!showExpenseForm);
+    }
+
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseHandler}/>
+            <button onClick={() => {
+                handleOnExpenseAddition();
+            }}
+                    hidden={showExpenseForm}
+            >Add New Expense
+            </button>
+
+            {showExpenseForm ? expenseForm : <div/>}
         </div>
     );
 }
